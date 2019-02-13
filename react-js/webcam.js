f6268b49d2834a202166427b61d04d2e65a81bed
Al Mamun Khan <almamunkhan04@gmail.com>
2019-02-13 13:50:31 +0600

    react webcam access

---
+++ react-js/webcam.js
@@ -0,0 +1,44 @@
+if (navigator.getUserMedia) {   // navigator.getUserMedia pass three arguments
+            navigator.getUserMedia({video: true}, //if want audio can be true 
+                stream => {
+                    let camera = document.getElementById('camera'); //class or id for load stream on html
+                    camera.srcObject = stream;  //src of stream
+                    camera.onloadedmetadata = () => {
+                        camera.play();
+                    }
+                }, err => {
+                    console.log(err)
+                }
+            );
+        }
+        
+ // <video id="camera" width="640" height="480" autoPlay/>
+// if want a component
+// class Webcam extends Component {
+
+//     render() {
+
+//         if (navigator.getUserMedia) {
+//             navigator.getUserMedia({video: true},
+//                 stream => {
+//                     let camera = document.getElementById('camera');
+//                     camera.srcObject = stream;
+//                     camera.onloadedmetadata = () => {
+//                         camera.play();
+//                     }
+//                 }, err => {
+//                     console.log(err)
+//                 }
+//             );
+//         }
+
+
+//         return (
+//             <div>
+//                 <video id="camera" width="640" height="480" autoPlay/>
+//             </div>
+//         );
+
+
+//     }
+// }
