af0979b8b5e477bff91ee178869bbb8e82631c56
Al Mamun Khan <almamunkhan04@gmail.com>
2019-04-14 01:30:46 +0600

    Update Api.js

--- react-js/Api.js
+++ react-js/Api.js
@@ -12,17 +12,17 @@ export default {
             getAll: () => fetch(todos),  //url
             create: (data) => fetch(todos, data),  //url, data
             update: ({ id }, data) => fetch(todos + `/${id}`, data), //url, data
-            delete: ({ id }) => fetch(`${id}`)
+            delete: ({ id }) => fetch(todos + `${id}`)
         }
 
     },
     todosAxios() {
-        return {
+       return {
             getOne: ({id}) => axios.get(todos + `/${id}`), //id_url 
             getAll: () => axios.get(todos),
             create: (data) => axios.post(todos, data), //url, data
             update: ({ id }, data) => axios.put(todos + `/${id}`, data), //url, data
-            delete: ({ id }) => axios.delete(`${id}`)
+            delete: ({ id }) => axios.delete(todos + `/${id}`)//id_url
         }
     }
 }
