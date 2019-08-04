252e4c76cd8e40b1cd015b5a8d27124ee34e5311
mrxmamun <almamunkhan04@gmail.com>
2019-08-04 22:56:03 +0600

    added pubsub subscription and list of subscriber of topic

--- python/pubsub.py
+++ python/pubsub.py
@@ -16,8 +16,8 @@ def publish_messages(project_id, topic_name):
     """Publishes multiple messages to a Pub/Sub topic."""
     # [START pubsub_quickstart_publisher]
     # [START pubsub_publish]
-    project_id = "Your Project Id"
-    topic_name = "Your Topic Name"
+    project_id = "Your Google Cloud Project ID"
+    topic_name = "Your Pub/Sub topic name"
 
     publisher = pubsub_v1.PublisherClient()
     # The `topic_path` method creates a fully qualified identifier
@@ -35,3 +35,51 @@ def publish_messages(project_id, topic_name):
     print('Published messages.')
 
 publish_messages("project_id","topic_name")
+
+
+def list_subscriptions_in_topic(project_id, topic_name):
+    """Lists all subscriptions for a given topic."""
+    # [START pubsub_list_topic_subscriptions]
+
+    # TODO project_id = "Your Google Cloud Project ID"
+    # TODO topic_name = "Your Pub/Sub topic name"
+
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_name)
+
+    for subscription in publisher.list_topic_subscriptions(topic_path):
+        print(subscription)
+    # [END pubsub_list_topic_subscriptions]
+list_subscriptions_in_topic("project_id","topic_name" )
+
+def receive_messages(project_id, subscription_name):
+    """Receives messages from a pull subscription."""
+    # [START pubsub_subscriber_async_pull]
+    # [START pubsub_quickstart_subscriber]
+
+
+    # TODO project_id = "Your Google Cloud Project ID"
+    # TODO subscription_name = "Your Pub/Sub subscription name"
+
+    subscriber = pubsub_v1.SubscriberClient()
+    # The `subscription_path` method creates a fully qualified identifier
+    # in the form `projects/{project_id}/subscriptions/{subscription_name}`
+    subscription_path = subscriber.subscription_path(
+        project_id, subscription_name)
+
+    def callback(message):
+        print('Received message: {}'.format(message))
+        message.ack()
+
+    subscriber.subscribe(subscription_path, callback=callback)
+
+    # The subscriber is non-blocking. We must keep the main thread from
+    # exiting to allow it to process messages asynchronously in the background.
+    print('Listening for messages on {}'.format(subscription_path))
+    while True:
+        time.sleep(60)
+    # [END pubsub_subscriber_async_pull]
+    # [END pubsub_quickstart_subscriber]
+
+
+receive_messages("project_id", "subscription_name")
