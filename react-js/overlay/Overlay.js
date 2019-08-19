437809f27dbb6abb2117af23f5db2ffa9cba115e
Al Mamun Khan <almamunkhan04@gmail.com>
2019-08-20 00:45:07 +0600

    added overlay component

---
+++ react-js/overlay/Overlay.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "./Overlay.module.css";
+
+const Overlay = ({children, className, show, close}) => (
+    <div className={styles.overlay + " " + className} style={{display: show ? 'block' : 'none'}}>
+        <div className={styles.overlay} onClick={close}/>
+        <div className={styles.content}>
+        <span className={styles.closeButton} onClick={close}>
+        &times;
+              </span>
+
+        {children}
+        </div>
+
+    </div>
+);
+export default Overlay;
