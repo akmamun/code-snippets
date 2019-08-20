c1e37d305a32c894b7af2524363a9ff136a09ee4
Al Mamun Khan <almamunkhan04@gmail.com>
2019-08-20 23:46:03 +0600

    fixed overlay js

--- react-js/overlay/Overlay.js
+++ react-js/overlay/Overlay.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import styles from "./Overlay.module.css";
+import styles from "./Overlay.module.css"; 
 
 const Overlay = ({children, className, show, close}) => (
     <div className={styles.overlay + " " + className} style={{display: show ? 'block' : 'none'}}>
-        <div className={styles.overlay} onClick={close}/>
         <div className={styles.content}>
         <span className={styles.closeButton} onClick={close}>
-        &times;
-              </span>
+            &times;
+        </span>
 
         {children}
         </div>
-
     </div>
 );
 export default Overlay;
+
